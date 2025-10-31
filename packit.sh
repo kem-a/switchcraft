fdb64681aca4732207177021ba2f7a0344231da8
Arnis Kemlers <ai.iens@icloud.com>
2025-11-01 00:14:46 +0200

    Update .gitignore, switchcraft.desktop, and add packit.sh for build process

---
+++ packit.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+APP_NAME="switchcraft"
+VERSION="${VERSION:-1.0.0}"
+BUILD_ROOT="$(pwd)"
+BUILD_DIR="$BUILD_ROOT/builddir"
+STAGING_ROOT="$BUILD_ROOT/staging"
+DESTDIR="$STAGING_ROOT/$APP_NAME-$VERSION"
+DIST_DIR="$BUILD_ROOT/dist"
+APPDIR="$STAGING_ROOT/AppDir"
+
+rm -rf "$DESTDIR" "$APPDIR" "$DIST_DIR"
+mkdir -p "$DESTDIR" "$APPDIR" "$DIST_DIR"
+
+meson setup "$BUILD_DIR" --prefix /usr --buildtype release --wipe
+meson compile -C "$BUILD_DIR"
+meson install -C "$BUILD_DIR" --destdir "$DESTDIR"
+
+fpm -s dir -t rpm  -n "$APP_NAME" -v "$VERSION" --license GPL-3.0-or-later \
+    --description "GNOME theme switch command runner" \
+    --url "https://github.com/kem-a/switchcraft" \
+    --chdir "$DESTDIR" --prefix / -p "$DIST_DIR"
+fpm -s dir -t deb  -n "$APP_NAME" -v "$VERSION" --license GPL-3.0-or-later \
+    --description "GNOME theme switch command runner" \
+    --url "https://github.com/kem-a/switchcraft" \
+    --chdir "$DESTDIR" --prefix / -p "$DIST_DIR"
+
+cp -a "$DESTDIR/usr" "$APPDIR/"
+install -Dm755 "$BUILD_DIR/switchcraft" "$APPDIR/usr/bin/$APP_NAME"
+install -Dm755 "$BUILD_DIR/switchcraft" "$APPDIR/AppRun"
+install -Dm644 "$DESTDIR/usr/share/applications/switchcraft.desktop" "$APPDIR/switchcraft.desktop"
+install -Dm644 "$DESTDIR/usr/share/icons/hicolor/256x256/apps/switchcraft.png" "$APPDIR/switchcraft.png"
+
+desktop-file-validate "$APPDIR/switchcraft.desktop"
+chrpath -d "$APPDIR/usr/bin/$APP_NAME" || true
+appimagetool "$APPDIR" "$DIST_DIR/${APP_NAME}-${VERSION}.AppImage"
